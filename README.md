e0c37686e34c34134bc42f38a01a88b4af02b4cc
Musab <musabnazir@gmail.com>
2022-12-03 17:37:12 -0500

    day1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Advent-of-Code-2021
+Repo where I attempt (poorly) to solve AOC2022 questions
---
+++ src/aoc_2022/day01.clj
@@ -0,0 +1,14 @@
+(ns aoc-2022.day01
+  (:require [clojure.string :as str]))
+
+;; Load data
+(def calories-input (str/split (slurp "./resources/day1_input.edn") #"\n\n"))
+
+(def elf-groups-str (mapv str/split-lines calories-input))
+
+(def elf-totals (reduce (fn [acc elf-calories]
+                          (let [elf-calories-int (mapv #(Integer/parseInt %) elf-calories)]
+                            (conj acc (reduce + elf-calories-int)))) [] elf-groups-str))
+
+(apply max elf-totals)
+
